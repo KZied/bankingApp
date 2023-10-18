2ac1d23ff15c7f14c74e53d4b4bf0b03ec54fc65
Zied <zied.kannou15@gmail.com>
2023-10-18 12:52:37 +0200

    global exceptions manager implementation

---
+++ src/main/java/com/zied/bankingApp/handlers/ExceptionRepresentation.java
@@ -0,0 +1,17 @@
+package com.zied.bankingApp.handlers;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionRepresentation {
+
+    private String errorMessage;
+    private String errorSource;
+    private Set<String> validationErrors;
+}
---
+++ src/main/java/com/zied/bankingApp/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.zied.bankingApp.handlers;
+
+import com.zied.bankingApp.exceptions.ObjetValidationException;
+import com.zied.bankingApp.exceptions.OperationNonPermittedException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ObjetValidationException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(ObjetValidationException exception){
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage("Object not valid exception has occured")
+                .errorSource(exception.getViolationSource())
+                .validationErrors(exception.getViolations())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(representation);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(EntityNotFoundException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(representation);
+    }
+
+    @ExceptionHandler(OperationNonPermittedException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(OperationNonPermittedException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(representation);
+    }
+
+}
