33b066467eddd19bd85420a80cc0e634cfbbeeca
Zied <zied.kannou15@gmail.com>
2023-10-22 14:34:30 +0200

    Security with JWT token part 1
    implementing UserDetails interface

--- src/main/java/com/zied/bankingApp/models/User.java
+++ src/main/java/com/zied/bankingApp/models/User.java
@@ -6,7 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -15,7 +20,7 @@ import java.util.List;
 @SuperBuilder
 @Entity
 @Table(name = "_user")
-public class User extends AbstractEntity{
+public class User extends AbstractEntity implements UserDetails {
 
     private String firstName;
 
@@ -43,4 +48,33 @@ public class User extends AbstractEntity{
     @OneToOne
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
 }
